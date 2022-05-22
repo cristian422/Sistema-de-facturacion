86abed3beffe95f3fd246ef59499c2c7b5e4249f
nickvillamizar <101729444+nickvillamizar@users.noreply.github.com>
2022-05-21 23:20:36 -0500

    carpeta con clases en python nicolas

---
+++ SISTEMAS_CLASES/clases nicolas python/Empresa.py
@@ -0,0 +1,6 @@
+import sucursal
+class Empresa:
+        sucursal:sucursal
+        nit:str
+        def crearsucursal(Direccion) -> str:
+             pass
---
+++ SISTEMAS_CLASES/clases nicolas python/PagoPse.py
@@ -0,0 +1,7 @@
+class PagoPse:
+        dispositivo: str
+        banco: str
+        CorreoElectronico: str
+         
+        def PagarPse(PagoPse:int) -> True:
+             pass
