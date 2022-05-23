504f2fa7efabed2d42717c633aa61f0a427ff96a
nickvillamizar <101729444+nickvillamizar@users.noreply.github.com>
2022-05-23 16:58:14 -0500

    Update sucursal.py

--- SISTEMAS_CLASES/clases nicolas python/sucursal.py
+++ SISTEMAS_CLASES/clases nicolas python/sucursal.py
@@ -5,5 +5,21 @@ class sucursal:
     direccion:string
     nombre_sucursal:string
     cajas:string()
-    
+    def__init__(self, direccion:string,nombre_sucursal:string,cajas:string):
+        self.direccion:direccion
+        self.nombre_sucursal:nombre_sucursal
+        self.cajas:string    
+            
+    def Getdireccion(self):
+        return self.direccion
+    def Setdireccion(self,direccion):
+        self.direccion=direccion
+    def Getnombre_sucursal(self):
+        return self.nombre_sucursal
+    def Setnombre_sucursal(self,nombre_sucursal):
+        self.nombre_sucursal=nombre_sucursal
+    def Getncajas(self):
+        return self.cajas
+    def Setcajas(self,cajas):
+        self.cajas=cajas
     
