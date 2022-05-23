03f4a93b7bfaa9f5f3d824e7b4afe01c82ffddd3
nickvillamizar <101729444+nickvillamizar@users.noreply.github.com>
2022-05-23 17:01:48 -0500

    Update PagoEfectivo.py

--- SISTEMAS_CLASES/clases nicolas python/PagoEfectivo.py
+++ SISTEMAS_CLASES/clases nicolas python/PagoEfectivo.py
@@ -2,5 +2,9 @@ class PagoEfectivo:
         devuelto:int
         def PagarEfectivo(TotalPagar:int) -> True:
             pass
-        
+        def Getdevuelto(self):
+        return self.devuelto
+        def Setdevuelto(self,devuelto):
+        self.devuelto=devuelto
+    
         
