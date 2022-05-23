07951df3ae109ab100894171bf9021a4ad5cb9e9
nickvillamizar <101729444+nickvillamizar@users.noreply.github.com>
2022-05-23 17:24:55 -0500

    Update PagoTarjeta.py

--- SISTEMAS_CLASES/clases nicolas python/PagoTarjeta.py
+++ SISTEMAS_CLASES/clases nicolas python/PagoTarjeta.py
@@ -8,5 +8,26 @@ class PagoTarjeta:
       franquicias: str
       def PagarTarjeta(TotalPagar:int) -> True:
         pass
+      def Getcodigo(self):
+        return self.codigo
+      def Setcodigo(self,codigo):
+        self.codigo=codigo
+      def Getbanco(self):
+        return self.banco
+      def Setbanco(self,banco):
+        self.banco=banco
+      def GetNombreBanco(self):
+        return self.NombreBanco
+      def SetNombreBanco(self,NombreBanco):
+        self.NombreBanco=NombreBanco
+      def Getfranquicias(self):
+        return self.franquicias
+      def Setfranquicias(self,franquicias):
+        self.franquicias=franquicias
+    
+    
+    
+    
+
 
       
