b1d2988a4cdc84fb4a28ec3fe4e40ef29972815f
nickvillamizar <101729444+nickvillamizar@users.noreply.github.com>
2022-05-23 17:07:58 -0500

    Update PagoPse.py

--- SISTEMAS_CLASES/clases nicolas python/PagoPse.py
+++ SISTEMAS_CLASES/clases nicolas python/PagoPse.py
@@ -5,3 +5,18 @@ class PagoPse:
          
         def PagarPse(PagoPse:int) -> True:
              pass
+        def Getdispositivo(self):
+        return self.dispositivo
+        def Setdispositivo(self,dispositivo:
+        self.dispositivo=dispositivo
+        def Getbanco(self):
+        return self.banco
+        def Setbanco(self,banco):
+        self.banco=banco
+        def GetCorreoElectronico(self):
+        return self.CorreoElectronico
+        def SetCorreoElectronico(self,CorreoElectronico):
+        self.CorreoElectronico=CorreoElectronico
+                  
+                           
+    
